fa23c82f9042c080894ef40cf7e49c71c6376cd3
dp.sokolov <dp.sokolov@jet.su>
2019-11-11 14:24:42 +0300

    update model

--- api/models.py
+++ api/models.py
@@ -4,7 +4,13 @@ from rest_framework import serializers
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
-    parent = models.ForeignKey('Category', null=True, related_name='children', on_delete=models.CASCADE)
+    parent = models.ForeignKey(
+        'Category',
+        blank=True,
+        null=True,
+        related_name='children',
+        verbose_name='parent',
+        on_delete=models.CASCADE)
 
 
 class CategorySerializer(serializers.ModelSerializer):
