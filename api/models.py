6e1f0073841c9380df2d5f2e1ba104900b247080
dp.sokolov <dp.sokolov@jet.su>
2019-11-12 15:24:33 +0300

    recursive sql request

--- api/models.py
+++ api/models.py
@@ -1,52 +1,71 @@
 from django.db import models, connection
 from rest_framework import serializers
 
-
+# SQLite3 dialect !!!
 SQL_REQUEST = """
-select id, name, parent_id
-from api_category
-where id = {pk}
-
-union
-
-select id, name, parent_id
-from api_category
-where id = (select parent_id from api_category where id = {pk})
-
-union
-
-select id, name, parent_id
-from api_category
-where parent_id = {pk}
-
-union
-
-select id, name, parent_id
-from api_category
-where parent_id = (select parent_id from api_category where id = {pk})
+WITH RECURSIVE
+    head (id, name, parent_id, level) AS
+        (
+            SELECT t.id, t.name, t.parent_id, 'head' AS LEVEL
+            FROM api_category AS t
+            WHERE t.id = {pk}
+        ),
+    children (id, name, parent_id, level) AS
+        (
+            SELECT id, name, parent_id, level
+            FROM head
+            UNION ALL
+            SELECT a.id, a.name, a.parent_id, 'children' AS LEVEL
+            FROM api_category AS a
+                     JOIN children AS c ON c.id = a.parent_id
+        ),
+    parents (id, name, parent_id, level) AS
+        (
+            SELECT id, name, parent_id, 'parents' AS LEVEL
+            FROM api_category as a
+            WHERE a.id IS (SELECT parent_id FROM head)
+            UNION ALL
+            SELECT a.id, a.name, a.parent_id, level
+            FROM api_category AS a
+                     JOIN parents ON parents.parent_id = a.id
+        ),
+    siblings (id, name, parent_id, level) AS
+        (
+            SELECT id, name, parent_id, 'siblings' AS LEVEL
+            FROM api_category
+            WHERE parent_id != (SELECT parent_id FROM api_category
+            WHERE id = {pk})
+        )
+select * from head
+UNION
+select * from children
+UNION  ALL
+select * from parents
+UNION ALL
+select * from siblings
 """
 
 
 class Relatives(models.Manager):
     def load(self, pk: int = None) -> tuple:
+        head_id = head_name = None
+        siblings = []
+        parents = []
+        children = []
         with connection.cursor() as cursor:
             cursor.execute(SQL_REQUEST.format(pk=pk))
-            result_dict = {}
             for row in cursor.fetchall():
                 category = self.model(id=row[0], name=row[1], parent_id=row[2])
-                result_dict[row[0]] = category
-        if not result_dict:
-            return None, None, [], [], []
-        head = result_dict.pop(int(pk))
-        parent = result_dict.pop(head.parent_id) if head.parent_id else None
-        children = siblings = []
-        for row in result_dict.values():
-            if row.parent_id == head.id:
-                children.append(row)
-            else:
-                siblings.append(row)
-
-        return head.id, head.name, [parent] if parent else [], children, siblings
+                if row[3] == 'head':
+                    head_id = category.id
+                    head_name = category.name
+                elif row[3] == 'parents':
+                    parents.append(category)
+                elif row[3] == 'children':
+                    children.append(category)
+                else:
+                    siblings.append(category)
+        return head_id, head_name, parents, children, siblings
 
 
 class Category(models.Model):
