8c7d35809e86a93b7f3054561b8a1727444931d0
dp.sokolov <dp.sokolov@jet.su>
2019-11-18 11:09:19 +0300

    comment out pure django

--- api/views.py
+++ api/views.py
@@ -31,6 +31,7 @@ class CategoryViewSet(ViewSet):
             return Response(None, status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
+        # Raw recursive SQL request version:
         categories = Category.relatives.load(pk=pk)
         result = dict((k, v) for k, v in zip(field_names, categories))
         if result['id'] is None:
@@ -38,8 +39,15 @@ class CategoryViewSet(ViewSet):
 
         return Response(RetrieveCategoriesSerializer(result).data)
 
+        # Pure Django ORM version:
         # category = Category.objects.select_related('parent').prefetch_related('children').get(pk=pk)
         # parents = list(category.get_parents())
         # children = list(category.children.all())
         # siblings = list(category.get_siblings())
-        # return Response(None, status.HTTP_200_OK)
+        # return Response(RetrieveCategoriesSerializer({
+        #   "id": category.id,
+        #   "name": category.name,
+        #   "parents": parents,
+        #   "children": children,
+        #   "siblings": siblings,
+        # }).data, status.HTTP_200_OK)
